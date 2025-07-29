60bd7d9b9b7a3bdd42e152d8127118245a7c2e74
woodg9461 <woodg9461@gmail.com>
2025-07-29 20:06:20 +0500

    Update workflow and fix version

--- update_metadata/__init__.py
+++ update_metadata/__init__.py
@@ -6,7 +6,7 @@ including version control integration, author detection, and bulk processing.
 """
 
 # Version of the package
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 # Import core functionality
 from .core import (
