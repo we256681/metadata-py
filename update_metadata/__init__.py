7afc87245823926e825381910b39eb9c2d9aafd3
woodg9461 <woodg9461@gmail.com>
2025-07-29 20:35:47 +0500

    Fix workflow syntax error and update version to 1.0.7

--- update_metadata/__init__.py
+++ update_metadata/__init__.py
@@ -6,7 +6,7 @@ including version control integration, author detection, and bulk processing.
 """
 
 # Version of the package
-__version__ = "1.0.6"
+__version__ = "1.0.7"
 
 # Import core functionality
 from .core import (
